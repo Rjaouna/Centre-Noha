354e5118d9ad820224bfc9809ebd31d744d5cdf2
ridouanej <jaouna.ridouane@gmail.com>
2025-12-15 11:48:28 +0100

    reset password

--- src/Controller/UserProfileController.php
+++ src/Controller/UserProfileController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserProfileController extends AbstractController
 {
@@ -31,5 +32,54 @@ class UserProfileController extends AbstractController
 		return $this->render('user/profile.html.twig', [
 			'user' => $user,
 		]);
+	}
+
+
+	#[Route('/profil/change-password', name: 'app_change_password', methods: ['POST'])]
+	public function changePassword(
+		Request $request,
+		UserPasswordHasherInterface $passwordHasher,
+		EntityManagerInterface $em
+	): Response {
+
+		/** @var User $user */
+		$user = $this->getUser();
+
+		if (!$user) {
+			throw $this->createAccessDeniedException();
+		}
+
+		$currentPassword = $request->request->get('current_password');
+		$newPassword = $request->request->get('new_password');
+		$confirmPassword = $request->request->get('confirm_password');
+
+		// Vérifier mot de passe actuel
+		if (!$passwordHasher->isPasswordValid($user, $currentPassword)) {
+			$this->addFlash('danger', 'Mot de passe actuel incorrect.');
+			return $this->redirectToRoute('app_user_profile');
+		}
+
+		// Vérifier confirmation
+		if ($newPassword !== $confirmPassword) {
+			$this->addFlash('danger', 'Les mots de passe ne correspondent pas.');
+			return $this->redirectToRoute('app_user_profile');
+		}
+
+		// Vérifier complexité
+		if (!preg_match('/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/', $newPassword)) {
+			$this->addFlash('danger', 'Le mot de passe doit contenir au moins 8 caractères, avec lettres et chiffres.');
+			return $this->redirectToRoute('app_user_profile');
+		}
+
+		// Hash + save
+		$user->setPassword(
+			$passwordHasher->hashPassword($user, $newPassword)
+		);
+
+		$em->flush();
+
+		$this->addFlash('success', 'Mot de passe mis à jour avec succès.');
+
+		return $this->redirectToRoute('app_user_profile');
 	}
 }
