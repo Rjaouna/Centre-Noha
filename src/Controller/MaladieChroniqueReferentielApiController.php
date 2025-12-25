8e63e47446b2e5dd39b8b4806e8fa4c4fc232fa9
ridouanej <jaouna.ridouane@gmail.com>
2025-12-25 20:30:24 +0100

    fix probleme maladie chronique

--- src/Controller/MaladieChroniqueReferentielApiController.php
+++ src/Controller/MaladieChroniqueReferentielApiController.php
@@ -1,26 +1,26 @@
 <?php
+
 namespace App\Controller;
 
 use App\Entity\MaladieChronique;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use App\Repository\MaladieChroniqueRepository;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/api/maladies-chroniques')]
-class MaladieChroniqueApiController extends AbstractController
+#[Route('/api/referentiel/maladies-chroniques')]
+class MaladieChroniqueReferentielApiController extends AbstractController
 {
-	#[Route('/', methods: ['GET'])]
+	#[Route('', methods: ['GET'])]
 	public function list(MaladieChroniqueRepository $repo): JsonResponse
 	{
-		return $this->json(array_map(fn($m) => [
+		return $this->json(array_map(static fn(MaladieChronique $m) => [
 			'id' => $m->getId(),
 			'nom' => $m->getNom(),
-			'description' => $m->getDescription()
-		], $repo->findAll()));
+			'description' => $m->getDescription(),
+		], $repo->findBy([], ['nom' => 'ASC'])));
 	}
 
 	#[Route('', methods: ['POST'])]
@@ -29,7 +29,7 @@ class MaladieChroniqueApiController extends AbstractController
 		$data = json_decode($request->getContent(), true);
 
 		$m = new MaladieChronique();
-		$m->setNom($data['nom']);
+		$m->setNom($data['nom'] ?? '');
 		$m->setDescription($data['description'] ?? null);
 
 		$em->persist($m);
@@ -38,31 +38,33 @@ class MaladieChroniqueApiController extends AbstractController
 		return $this->json(['success' => true]);
 	}
 
-	#[Route('/{id}', methods: ['GET'])]
+	#[Route('/{id<\d+>}', methods: ['GET'])]
 	public function show(MaladieChronique $m): JsonResponse
 	{
 		return $this->json([
 			'id' => $m->getId(),
 			'nom' => $m->getNom(),
-			'description' => $m->getDescription()
+			'description' => $m->getDescription(),
 		]);
 	}
 
-	#[Route('/{id}', methods: ['PUT'])]
+	#[Route('/{id<\d+>}', methods: ['PUT'])]
 	public function update(
 		MaladieChronique $m,
 		Request $request,
 		EntityManagerInterface $em
 	): JsonResponse {
 		$data = json_decode($request->getContent(), true);
-		$m->setNom($data['nom']);
+
+		$m->setNom($data['nom'] ?? '');
 		$m->setDescription($data['description'] ?? null);
+
 		$em->flush();
 
 		return $this->json(['success' => true]);
 	}
 
-	#[Route('/{id}', methods: ['DELETE'])]
+	#[Route('/{id<\d+>}', methods: ['DELETE'])]
 	public function delete(
 		MaladieChronique $m,
 		EntityManagerInterface $em
