d214eca985bb635af2075f83f75097d6d52398ad
ridouanej <jaouna.ridouane@gmail.com>
2026-01-07 10:50:03 +0100

    ux design touts les page

--- public/index.php
+++ public/index.php
@@ -2,7 +2,9 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+date_default_timezone_set($_SERVER['APP_TIMEZONE'] ?? $_ENV['APP_TIMEZONE'] ?? 'Europe/Paris');
+
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
